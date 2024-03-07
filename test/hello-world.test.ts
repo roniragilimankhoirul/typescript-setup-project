b4cae57aac0a52c2e1bb5b02f8ea9a1ebd15a2e2
roniragilimankhoirul <ronisensei7@gmail.com>
2024-03-08 03:04:36 +0700

    initial commit

---
+++ test/hello-world.test.ts
@@ -0,0 +1,8 @@
+import { helloWorld } from "../src/hello-world";
+
+describe("Name of the group", () => {
+  it("should ", () => {
+    const result = helloWorld();
+    expect(result).toBe("hello world");
+  });
+});
